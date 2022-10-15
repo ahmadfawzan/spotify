22675250aa5e194f59c96afa30b95b5c4c96a81f
ahmadfawzan <ahmadhassan0566@gmail.com>
2022-10-15 22:03:19 +0300

    refactor

--- spotify/src/components/Search/Search.js
+++ spotify/src/components/Search/Search.js
@@ -67,7 +67,7 @@ export default function Search() {
     }
   }, [params]);
   /* console.log(params.search);  */
-  /* refactor test 2*/
+  /* refactor */
   function millisToMinutesAndSeconds(millis) {
     var minutes = Math.floor(millis / 60000);
     var seconds = ((millis % 60000) / 1000).toFixed(0);
